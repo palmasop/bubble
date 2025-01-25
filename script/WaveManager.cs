1c5c475793f08eb6efb67ee4de5ce4701b8015ec
~~ <opfk00@gmail.com>
2025-01-26 05:53:01 +0800

    udpated background scrolling

--- script/WaveManager.cs
+++ script/WaveManager.cs
@@ -2,16 +2,32 @@ using Godot;
 using System.Collections.Generic;
 
 [Tool]
-public partial class WaveManager : Node
+public partial class WaveManager : Node2D
 {
+	[Export] TextureRect waveBackground;
 	[Export] public NodePath[] waveSpawners; // Array of paths to EnemySpawner nodes
 	[Export] public float waveInterval = 10.0f; // Time between waves
 
 	private int _currentWave = 0;
 	private float _waveTimer = 0.0f;
+	private float viewportHeight;
+
+	public override void _Ready()
+	{
+		viewportHeight = GetViewportRect().Size.Y;
+	}
 
 	public override void _Process(double delta)
 	{
+		if (Engine.IsEditorHint()) return;
+
+		if (waveBackground.Position.Y <= viewportHeight)
+		{
+			Vector2 currentPos = waveBackground.Position;
+			currentPos.Y = Mathf.Lerp(currentPos.Y, 0, (float)delta / (waveInterval * waveSpawners.Length));
+			waveBackground.Position = currentPos;
+		}
+
 		_waveTimer += (float)delta;
 
 		if (_waveTimer >= waveInterval)
